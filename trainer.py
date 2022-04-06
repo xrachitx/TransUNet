fb11885cd74fa07c4555aa3683a60ee3fdc8db5f
xrachitx <45661647+xrachitx@users.noreply.github.com>
2022-04-07 01:32:32 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -53,7 +53,7 @@ def trainer_synapse(args, model, snapshot_path):
     iterator = tqdm(range(max_epoch), ncols=70)
     for epoch_num in iterator:
         for i_batch, sampled_batch in enumerate(trainloader):
-            image_batch, label_batch,weights,_ = sampled_batch['image'], sampled_batch['label']
+            image_batch, label_batch,weights,_ = sampled_batch[0], sampled_batch[1],sampled_batch[2],sampled_batch[3]
             image_batch, label_batch = image_batch.cuda(), label_batch.cuda()
             outputs = model(image_batch)
             print(outputs.shape)
