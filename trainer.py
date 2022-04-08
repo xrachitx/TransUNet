7ff10eab41f121b978e9d536e28476186d613700
xrachitx <45661647+xrachitx@users.noreply.github.com>
2022-04-08 19:32:58 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -56,8 +56,8 @@ def trainer_synapse(args, model, snapshot_path):
             image_batch, label_batch,weights,_ = sampled_batch[0], sampled_batch[1],sampled_batch[2],sampled_batch[3]
             image_batch, label_batch = image_batch.cuda(), label_batch.cuda()
             outputs = model(image_batch)
-            print(outputs.shape)
-            exit()
+#             print(outputs.shape)
+#             exit()
             loss_ce = ce_loss(outputs, label_batch[:].long())
             loss_dice = dice_loss(outputs, label_batch, softmax=True)
             loss = 0.5 * loss_ce + 0.5 * loss_dice
