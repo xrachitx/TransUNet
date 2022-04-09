2989fd03b88eaa6c41396e911c4a3a2257121359
xrachitx <45661647+xrachitx@users.noreply.github.com>
2022-04-10 00:20:13 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -73,7 +73,7 @@ def trainer_synapse(args, model, snapshot_path):
             image_batch, label_batch,weights,_ = sampled_batch[0], sampled_batch[1],sampled_batch[2],sampled_batch[3]
             image_batch, label_batch,weights = image_batch.cuda(), label_batch.cuda().squeeze(1),weights.cuda()
             outputs = model(image_batch).squeeze(1)
-            print(outputs.shape,label_batch[:].long().shape,weights)
+            print(outputs.shape,label_batch[:].long().shape,weights,label_batch.shape)
             print(weights.shape)
 #             exit()
             loss_ce = ce_loss(outputs, label_batch[:].long(),weights)
