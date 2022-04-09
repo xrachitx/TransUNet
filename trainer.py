632aabbad5481178491529c5ca2a270041778c5d
xrachitx <45661647+xrachitx@users.noreply.github.com>
2022-04-10 00:22:27 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -71,12 +71,12 @@ def trainer_synapse(args, model, snapshot_path):
     for epoch_num in iterator:
         for i_batch, sampled_batch in enumerate(trainloader):
             image_batch, label_batch,weights,_ = sampled_batch[0], sampled_batch[1],sampled_batch[2],sampled_batch[3]
-            image_batch, label_batch,weights = image_batch.cuda(), label_batch.cuda().squeeze(1),weights.cuda()
-            outputs = model(image_batch).squeeze(1)
+            image_batch, label_batch,weights = image_batch.cuda(), label_batch.cuda(),weights.cuda()
+            outputs = model(image_batch)
             print(outputs.shape,label_batch[:].long().shape,weights,label_batch.shape)
             print(weights.shape)
 #             exit()
-            loss_ce = ce_loss(outputs, label_batch[:].long(),weights)
+            loss_ce = ce_loss(outputs.squeeze(1), label_batch.squeeze(1)[:].long(),weights)
             loss_dice = dice_loss(outputs, label_batch, softmax=True)
             loss = 0.5 * loss_ce + 0.5 * loss_dice
             optimizer.zero_grad()
