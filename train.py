a7194f21a2b453ebcd9bde0944790981558c4681
xrachitx <45661647+xrachitx@users.noreply.github.com>
2022-04-12 23:56:36 +0530

    Update train.py

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ parser.add_argument('--num_classes', type=int,
 parser.add_argument('--max_iterations', type=int,
                     default=15000, help='maximum epoch number to train')
 parser.add_argument('--max_epochs', type=int,
-                    default=70, help='maximum epoch number to train')
+                    default=50, help='maximum epoch number to train')
 parser.add_argument('--batch_size', type=int,
                     default=24, help='batch_size per gpu')
 parser.add_argument('--n_gpu', type=int, default=1, help='total gpu')
