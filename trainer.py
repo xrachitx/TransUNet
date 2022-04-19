d9ab55966696c0c1a4fd7f10f96edc580d59bb17
xrachitx <45661647+xrachitx@users.noreply.github.com>
2022-04-20 01:08:17 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -44,7 +44,7 @@ def trainer_synapse(args, model, snapshot_path):
     num_classes = args.num_classes
     batch_size = args.batch_size * args.n_gpu
     # max_iterations = args.max_iterations
-    db_train = LoadData(args.list_dir,args.root_path)
+    db_train = LoadData(args.list_dir,args.root_path,args.double_channel)
 #     db_train = Synapse_dataset(base_dir=args.root_path, list_dir=args.list_dir, split="train",
 #                                transform=transforms.Compose(
 #                                    [RandomGenerator(output_size=[args.img_size, args.img_size])]))
