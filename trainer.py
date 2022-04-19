fd3304cbe3063be44fa774fedb3d505e04053d43
xrachitx <45661647+xrachitx@users.noreply.github.com>
2022-04-20 02:30:54 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -46,9 +46,10 @@ def BCELoss_class_weighted():
             weights = torch.unsqueeze(weights,axis=2)
             weights = torch.unsqueeze(weights,axis=3)
             weights = torch.tile(weights,(1,1,inpt.shape[-2],inpt.shape[-1]))
-            bce = - weights[:,0,:,:] * target[:,1,:,:] * torch.log(inpt[:,1,:,:]) - (target[:,0,:,:]) * weights[:,1,:,:] * torch.log(inptinpt[:,0,:,:])
+            bce = - weights[:,0,:,:] * target[:,1,:,:] * torch.log(inpt[:,1,:,:]) - (target[:,0,:,:]) * weights[:,1,:,:] * torch.log(inpt[:,0,:,:])
+            return torch.mean(bce)
 
-    return loss
+        return loss
 
 def trainer_synapse(args, model, snapshot_path):
     
