b95c8b76653b31e097efaf3bccbc13630d640549
xrachitx <45661647+xrachitx@users.noreply.github.com>
2023-09-26 01:28:57 +0530

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -23,7 +23,7 @@ class LoadData(Dataset):
 
         inputName = os.path.join(self.rootDir, self.frame.iloc[idx, 0][1:])
         targetName = os.path.join(self.rootDir, self.frame.iloc[idx, 1][1:])
-        # print(inputName,targetName,self.rootDir, self.frame.iloc[idx, 1][1:])
+        print(inputName,targetName,self.rootDir, self.frame.iloc[idx, 0])
         inputImage = cv2.imread(inputName)
         targetImage = cv2.imread(targetName, cv2.IMREAD_GRAYSCALE)
         targetImage = targetImage > 0.0
