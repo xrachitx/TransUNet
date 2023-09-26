a73f8b2eba212d834f1755e97c83f258b875c595
xrachitx <45661647+xrachitx@users.noreply.github.com>
2023-09-26 15:32:30 +0530

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -47,7 +47,7 @@ def BCELoss_class_weighted():
             weights = torch.tile(weights,(1,1,inpt.shape[-2],inpt.shape[-1]))
             print("in ce",weights[:,0,:,:].shape, target[:,1,:,:].shape)
             print(weights[:,0,:,:],target[:,1,:,:])
-            bce = - weights[:,0,:,:] * target[:,1,:,:] * torch.log(inpt[:,1,:,:]) - (target[0,:,:]) * weights[:,1,:,:] * torch.log(inpt[:,0,:,:])
+            bce = - weights[:,0,:,:] * target[:,1,:,:] * torch.log(inpt[:,1,:,:]) - (target[:,0,:,:]) * weights[:,1,:,:] * torch.log(inpt[:,0,:,:])
             return torch.mean(bce)
     return loss
 
